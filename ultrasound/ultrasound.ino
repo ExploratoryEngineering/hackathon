ebf5a3d9ba115c60217d4767c0836de90c36f6fb
Bjorn Borud <bborud@gmail.com>
2018-04-20 01:21:36 +0200

    Added some more output and comments.

--- ultrasound/ultrasound.ino
+++ ultrasound/ultrasound.ino
@@ -3,7 +3,13 @@
  * ultrasound sensor and send the distance measured over NB-IoT using the 
  * EE-NBIOT-01 breakout board.
  *
- * In order to use this code you need to install two libraries:
+ * In this example the payload is formatted as a JSON message. In real
+ * life you would most likely make use of a binary format for
+ * compactness.
+ *
+ *
+ * In order to use this code you need to install two libraries in your
+ * Arduino IDE:
  *
  *     ArduinoNBIoT 
  *     ------------
@@ -35,7 +41,11 @@
 // Maximum distance we will be measuring expressed in centimeters.
 // Note that if the distance you are measuring exceeds this distance
 // any measurement performed will be 0.
-#define MAX_DISTANCE_CM 260 
+#define MAX_DISTANCE_CM                  260
+
+// How long between each message we send.  If you send too often the
+// connection might get throttled so keep it civil.
+#define MILLISECONDS_BETWEEN_MESSAGES 15000
 
 // These pins are used to communicate with the NB-IoT Breakout board.
 #define RX_PIN 10
@@ -51,7 +61,7 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE_CM);
 
 // If the message you send back is longer than 60 bytes you must
 // adjust this setting appropriately.
-#define BUFFER_SIZE 80
+#define BUFFER_SIZE 50
 
 // Buffer into which we render the response we send back 
 char buffer[BUFFER_SIZE];
@@ -66,9 +76,26 @@ void setup() {
     while (!Serial) {
         // Wait for the serial connection with the console to be established.
     }
+
+    Serial.println("Booting");
+    Serial.println("----------------------------------------");
     
     // Initialize the NB-IoT breakout board
+    Serial.println(" - Initializing NB-IoT");
     nbiot.begin();
+
+    // Print some data on the device
+    nbiot.i64toa(nbiot.imei(), buffer);
+    Serial.print(" - IMEI: ");
+    Serial.println(buffer);
+
+    nbiot.i64toa(nbiot.imsi(), buffer);
+    Serial.print(" - IMSI: ");
+    Serial.println(buffer);
+
+    Serial.println(nbiot.connected() ? " - Connected to network" : " - Not connected to network");
+
+    Serial.println("----------------------------------------");
 }
     
 // For as long as the device is powered up the loop() function will be called
@@ -76,29 +103,36 @@ void setup() {
 void loop() {
     // Send out a ping and record the distance in centimeters.
     unsigned long distance = sonar.ping_cm();
-    
-    // Prepare buffer with message and print it on the console so you can see
-    // what is being sent.
-    sprintf(buffer, "{\"dist\" : %lu}", distance);
+    Serial.print("*** Measured distance to ");
+    Serial.print(distance);
+    Serial.println("cm");
+
+    // Format the message
+    int len = snprintf(buffer, BUFFER_SIZE, "{\"dist\" : %lu}", distance);
+    Serial.print("*** Sending message: ");
     Serial.println(buffer);
    
-    // Send the contents of buffer over NB-IoT.
+    // Create a socket for sending the message
     if (! nbiot.createSocket()) {
-        Serial.println("Failed to create socket.  Will retry in 5 seconds");
+        Serial.println("*** Failed to create socket.  Will retry in 5 seconds");
         delay(5000);
         return;
     }
-    
+
+    // Send the buffer via NB-IoT
     if (nbiot.send(buffer, strlen(buffer))) {
-        Serial.println("Succeeded in sending message");
+        Serial.println("*** Succeeded in sending message");
     } else {
-        Serial. println("Failed to send message");
+        Serial. println("*** Failed to send message");
     }
     
     // Close the socket.
     nbiot.closeSocket();
     
     // Wait for 10.000 milliseconds, which is 10 seconds
-    delay(10000);
+    Serial.print("*** Waiting for ");
+    Serial.print(MILLISECONDS_BETWEEN_MESSAGES / 1000);
+    Serial.println(" seconds");
+    delay(MILLISECONDS_BETWEEN_MESSAGES);
 }
     
